1c279e8c4b8f5ae404b999b621b574b14aa66ca3
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-08-30 07:53:00 -0500

    finish chapter 6

---
+++ chapter-6/dmalloc.c
@@ -0,0 +1,37 @@
+/*
+    dmalloc -l logfile -i 100 low
+    gcc -DDMALLOC dmalloc.c -ocaesar -ldmalloc
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifdef DMALLOC
+#include "dmalloc.h"
+#endif
+
+void usage(char *msg) {
+    fprintf(stderr, "%s", msg);
+    free(msg);
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3 && argc != 4) {
+        /* the error message won't be more than 80 chars */
+        char *errmsg = (char *)malloc(80);
+        sprintf(
+            errmsg,
+            "Sorry %s, \nUsage: caesar secret_file keys_file [output_file]\n",
+            getenv("USER")
+        );
+        usage(errmsg);
+        free(errmsg);
+        exit(EXIT_FAILURE);
+    }
+
+    // ...
+
+    exit(EXIT_SUCCESS);
+}
