24a8add1ae71e835ecf3973b172c604bf61b759c
mbmcmullen27 <68041054+mbmcmullen27@users.noreply.github.com>
2024-02-04 09:27:44 -0600

    Revert "Update matrix.c"
    This reverts commit 78136ae27a698214993fe98c55b4f4619066879d.

--- chapter-6/matrix.c
+++ chapter-6/matrix.c
@@ -3,13 +3,6 @@
 #include <string.h>
 #include <stdint.h>
 
-#define ARRAY_LEN(a) (sizeof(a) / sizeof((a)[0]))
-#define MATRIX_ROW_LEN(m) (ARRAY_LEN(m))
-#define MATRIX_COL_LEN(m) (ARRAY_LEN((m)[0]))
-
-#define MATRIX_SUM(m) matrix_sum(MATRIX_ROW_LEN(m), m)
-#define MATRIX_PRINT(m) matrix_print(MATRIX_ROW_LEN(m), m)
-
 int matrix_sum(size_t rows, int m[][4]) {
   int sum = 0;
   for (size_t i = 0; i < rows; i++) {
@@ -20,15 +13,6 @@ int matrix_sum(size_t rows, int m[][4]) {
   return sum;
 }
 
-static void matrix_print(size_t rows, int m[][4]) {
-  for (size_t i = 0; i < rows; i++) {
-    for (size_t j = 0; j < 4; j++) {
-      printf("%3d ", m[i][j]);
-    }
-    puts("");
-  }
-}
-
 int main(void) {
   int m1[5][4] = {
     {1, 1, 1, 1},
@@ -37,17 +21,9 @@ int main(void) {
     {1, 1, 1, 1},
     {1, 1, 1, 1}
   };
-  int m2[25][4] = { 0 };
+  int m2[100][4] = { 0 };
   int m3[2][4] = { 0 };
-
-  /* fill m2 with numbers from 1 to sizeof(m2), i.e. the expected sum is 5050 */
-  for (size_t i = 0; i < MATRIX_ROW_LEN(m2); i++)
-    for (size_t j = 0; j < MATRIX_COL_LEN(m2); j++)
-      m2[i][j] = i * MATRIX_COL_LEN(m2) + j + 1;
-
-  MATRIX_PRINT(m2);
-
-  printf("m1 = %d.\n", MATRIX_SUM(m1));
-  printf("m2 = %d.\n", MATRIX_SUM(m2));
-  printf("m3 = %d.\n", MATRIX_SUM(m3));
+  printf("m1 = %d.\n", matrix_sum(5, m1));
+  printf("m2 = %d.\n",  matrix_sum(100, m2));
+  printf("m3 = %d.\n", matrix_sum(2, m3));
 }
